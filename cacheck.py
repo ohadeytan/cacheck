4a9804f649dcc9fa0283a3003c032bcf7dc58069
Ohad Eytan <OhadEy@gmail.com>
2018-08-20 17:38:32 +0300

    measure time

--- cacheck.py
+++ cacheck.py
@@ -8,12 +8,14 @@ def run(trace, policy):
     return policy.get_stats()
 
 def main():
-    print("{:<12} {:<12} {:<12} {:<12}".format('Name', 'Hits', 'Misses', 'Hit Ratio'))
+    print("{:<12} {:<12} {:<12} {:<12} {:<12}".format('Name', 'Hits', 'Misses', 'Hit Ratio', 'Time(s)'))
     policies = [LRU(1000), WTinyLFU(1000), WI_WTinyLFU(1000), WC_WTinyLFU(1000)]
     for policy in policies:
+        start = time.time()
         trace = LirsParser('sample_trace.tr')
         results = run(trace, policy)
-        print("{name:<12} {hits:<12} {misses:<12} {hit ratio:<12}".format(**results))
+        end = time.time()
+        print("{name:<12} {hits:<12} {misses:<12} {hit ratio:<12} {time:<12}".format(**results, time=end - start))
 
 if __name__ == "__main__":
     main()
