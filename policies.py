1b2d8ea5892529d654de250bf3c6a4a30c62b658
Ohad Eytan <ohadey@gmail.com>
2018-11-27 18:47:50 +0200

    add a preliminary size-aware W-TinyLFU

--- policies.py
+++ policies.py
@@ -1,9 +1,11 @@
 import pyximport; pyximport.install()
 from enum import Enum, auto
 from cms import CMS
-from collections import Counter
+from collections import Counter, defaultdict
 from math import log
 from scipy import stats
+
+debug = False 
 
 class Policy(object):
     def __init__(self, maximum_size):
@@ -213,8 +215,123 @@ class Indicator(object):
         self.hinter_count = 0
         self.freqs.clear()
 
+#Sized WTinyLFU
+class SizedWTinyLFU(Policy):
+    def __init__(self, maximum_size, window_percentage=1):
+        super().__init__(maximum_size)
+
+        self.data = {}
+        self.data_size = 0
+
+        self.freqs = FreqsCounter(maximum_size)
+
+        self.sentinel_window = Node()    # LRU
+        self.sentinel_probation = Node() # SLRU
+        self.sentinel_protected = Node() # SLRU
+        
+        self.max_window_size = (self.maximum_size * window_percentage) // 100
+        max_main = self.maximum_size - self.max_window_size
+        self.max_protected = max_main * 4 // 5
+
+        self.size_window = 0
+        self.size_protected = 0
+
+    def record(self, key, size=1):
+        self.freqs.increment(key, size)
+        node = self.data.get(key)
+        if not node:
+            self.misses += 1
+            if size > self.max_protected:
+                return False
+            new_node = Node(key, Node.Status.Window, size)
+            if size <= self.max_window_size:
+                new_node.append_to_tail(self.sentinel_window)
+            else:
+                new_node.append_to_head(self.sentinel_window) # Evicted immediately
+            self.data[key] = new_node
+            self.data_size += size
+            self.size_window += size
+            if self.size_window > self.max_window_size:
+                self.evict()
+            return False
+        else:
+            self.hits += 1
+            node.remove()
+            if node.status == Node.Status.Window:
+                node.append_to_tail(self.sentinel_window)
+            elif node.status == Node.Status.Probation:
+                node.status = Node.Status.Protected
+                node.append_to_tail(self.sentinel_protected)
+                self.size_protected += size
+                self.demote_protected()
+            elif node.status == Node.Status.Protected:
+                node.append_to_tail(self.sentinel_protected)
+            return True
+
+    def demote_protected(self):
+        while self.size_protected > self.max_protected:
+            demote = self.sentinel_protected.next_node
+            demote.remove()
+            demote.status = Node.Status.Probation
+            demote.append_to_tail(self.sentinel_probation)
+            self.size_protected -= demote.size
+
+    def evict(self):
+        candidates, victims = [], []
+        candidates_size, victims_size = 0, 0
+        candidates_freq, victims_freq = 0, 0
+        while self.size_window > self.max_window_size:
+            candidate = self.sentinel_window.next_node
+            candidate.remove()
+            self.size_window -= candidate.size
+            candidates_size += candidate.size
+            candidates_freq += self.freqs.frequancy(candidate.data)
+            candidates.append(candidate)
+        needed_space = self.data_size - self.maximum_size 
+        victim = self.sentinel_probation.next_node
+        while needed_space > victims_size:
+            victims_size += victim.size
+            victims_freq += self.freqs.frequancy(victim.data)
+            victims.append(victim)
+            victim = victim.next_node
+            if victim == victim.next_node:
+                victim = self.sentinel_protected.next_node
+        if candidates_freq > victims_freq:
+            for candidate in candidates:
+                candidate.status = Node.Status.Probation
+                candidate.append_to_tail(self.sentinel_probation)
+            for victim in victims:
+                del self.data[victim.data]
+                victim.remove()
+                if victim.status == Node.Status.Protected:
+                    self.size_protected -= victim.size
+            self.data_size -= victims_size
+        else:
+            for candidate in candidates:
+                del self.data[candidate.data]
+                candidate.remove()
+            self.data_size -= candidates_size
+
+class FreqsCounter(object):
+    def __init__(self, maximum_size):
+        next_power_of_two = 2**(len(bin(maximum_size-1))-2)
+        self.period = 10*next_power_of_two//50000
+        self.additions = 0
+        self.counter = defaultdict(int) 
+    def increment(self, key, size):
+        self.counter[key] += 1
+        self.additions += 1
+        if self.additions >= self.period:
+            if debug: print("Halving freqs")
+            for key in self.counter:
+                self.counter[key] = self.counter[key] >> 1
+            self.additions = self.additions >> 1
+    def frequancy(self, key):
+        return self.counter[key]
+
+# Node Object
 class Node(object):
-    def __init__(self, data=None, size=1, status=None):
+    def __init__(self, data=None, status=None, size=1):
         self.data = data
         self.next_node = self
         self.prev_node = self
